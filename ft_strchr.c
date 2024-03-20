4972e22ce8dfae41f3b6045f1bb61fc39ad26d48
Leo Agea <leoagea@gmail.com>
2024-03-20 19:05:27 +0100

    strchr v2

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,27 +6,19 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:01:58 by lagea             #+#    #+#             */
-/*   Updated: 2024/03/20 18:23:45 by lagea            ###   ########.fr       */
+/*   Updated: 2024/03/20 19:05:00 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *str, int c)
+char *ft_strchr(const char *str, int c)
 {
-	int			i;
-	char		a;
-    char        *ptr;
-	const int	len = ft_strlen(str);
-
-	i = 0;
-    ptr = str;
-	a = (char)c;
-	while (i <= len)
-	{
-		if (str[i] == a)
-			return (ptr = &str[i]);
-		i++;
-	}
-	return (NULL);
+    while ((char) c != *str)
+    {
+        if (!*str)
+            return NULL;
+        str++;
+    }
+    return ((char *)str);
 }
