76a90c4326dba2c760757d91e7177fabe512f163
Leo Agea <leoagea@gmail.com>
2024-03-20 23:42:51 +0100

    strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 23:22:52 by lagea             #+#    #+#             */
+/*   Updated: 2024/03/20 23:42:38 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t ft_strlcpy(char * dst, const char * src, size_t dstsize)
+{
+    size_t i;
+
+    i = 0;
+    if (!dst || !src)
+        return 0;
+	if (dstsize != 0)
+	{
+		while (i < dstsize - 1 && src [i])
+		{
+			dst [i] = src [i];
+			i++;
+		}
+		dst [i] = '\0';
+	}
+	return (ft_strlen(src));
+}
