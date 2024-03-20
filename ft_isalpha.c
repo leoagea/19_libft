a9b4014ce8ddec4893d1c6b0220becdc5b82a3af
Leo Agea <leoagea@gmail.com>
2024-03-20 02:50:07 +0100

    libft.h

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,13 +6,13 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:25:44 by lagea             #+#    #+#             */
-/*   Updated: 2024/03/20 02:39:18 by lagea            ###   ########.fr       */
+/*   Updated: 2024/03/20 02:46:08 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_is_alpha(int c)
+int	ft_isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:26:11 by lagea             #+#    #+#             */
-/*   Updated: 2024/03/20 02:19:03 by lagea            ###   ########.fr       */
+/*   Updated: 2024/03/20 02:44:12 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,14 @@
 
 #include <stddef.h>
 
-int ft_is_alpha (int c);
+int ft_isalpha (int c);
 int ft_isdigit (char c);
+int	ft_isascii(char c);
 int	ft_isalnum(char c);
 int	ft_isprint(char c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+
+size_t	ft_strlen(const char *s);
 
 #endif LIBFT_H
