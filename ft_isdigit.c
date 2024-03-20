8c91218c366b2550fed7032334a1544299d3f965
Agea Léo <lagea>
2024-03-20 01:49:21 +0100

    isalnum

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,13 +6,13 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:30:31 by lagea             #+#    #+#             */
-/*   Updated: 2024/03/20 01:36:46 by lagea            ###   ########.fr       */
+/*   Updated: 2024/03/20 01:48:34 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isdigit (char c)
+int	ft_isdigit(char c)
 {
-    if (c >= '0' && c <= '9')
-        return (1);
-    return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 01:26:11 by lagea             #+#    #+#             */
-/*   Updated: 2024/03/20 01:37:05 by lagea            ###   ########.fr       */
+/*   Updated: 2024/03/20 01:48:44 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 int ft_is_alpha (int c);
 int ft_isdigit (char c);
+int	ft_isalnum(char c);
 
 #endif LIBFT_H
