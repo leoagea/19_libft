56ae6903add275e2c6af3135d45bd11c9c936234
Leo Agea <leoagea@gmail.com>
2024-03-20 22:00:19 +0100

    memchr

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 21:45:42 by lagea             #+#    #+#             */
+/*   Updated: 2024/03/20 22:00:19 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    while (i < n)
+    {
+        if (((unsigned char *)s) [i] == (char) c)
+            return (((unsigned char *)s) + i);
+        i++;
+    }
+    return NULL;
+}
