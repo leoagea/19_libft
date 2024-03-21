6b707a087842e81af6f5087997be18da60426d94
Leo Agea <leoagea@gmail.com>
2024-03-21 19:48:17 +0100

    itoa

---
+++ ft_itoa.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 19:25:36 by lagea             #+#    #+#             */
+/*   Updated: 2024/03/21 19:47:45 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	size_t	len_nbr;
+	size_t	buf;
+	char	*nbr;
+
+	buf = n;
+	len_nbr = 1;
+	while (buf > 10)
+	{
+		buf /= 10;
+		len_nbr++;
+	}
+	nbr = (char *)malloc((len_nbr + 1) * sizeof(char));
+	nbr[len_nbr] = '\0';
+	while (n > 10)
+	{
+		len_nbr--;
+		nbr[len_nbr] = (n % 10) + 48;
+		n /= 10;
+	}
+	nbr[0] = (n % 10) + 48;
+	return (nbr);
+}
