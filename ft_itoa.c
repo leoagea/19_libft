d21cca93e28ca6095350709538347571179310c9
Leo Agea <leoagea@gmail.com>
2024-03-21 22:21:32 +0100

    handle negative

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,35 +6,51 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 19:25:36 by lagea             #+#    #+#             */
-/*   Updated: 2024/03/21 21:41:11 by lagea            ###   ########.fr       */
+/*   Updated: 2024/03/21 22:21:12 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /// TODO : gerer les nombres negatifs
+int	int_len(int n)
+{
+	size_t	len_nbr;
+
+	len_nbr = 0;
+	if (n < 0)
+	{
+		n = -n;
+		len_nbr = 1;
+	}
+	else if (n == 0)
+		len_nbr = 1;
+	while (n != 0)
+	{
+		n /= 10;
+		len_nbr++;
+	}
+	return (len_nbr);
+}
 
 char	*ft_itoa(int n)
 {
 	size_t	len_nbr;
-	size_t	buf;
 	char	*nbr;
 
-	buf = n;
-	len_nbr = 1;
-	while (buf > 10)
-	{
-		buf /= 10;
-		len_nbr++;
-	}
+	len_nbr = int_len(n);
 	nbr = (char *)malloc((len_nbr + 1) * sizeof(char));
+	if (n < 0)
+	{
+		n = -n;
+		nbr[0] = '-';
+	}
 	nbr[len_nbr] = '\0';
-	while (n > 10)
+	while (n != 0)
 	{
 		len_nbr--;
 		nbr[len_nbr] = (n % 10) + 48;
 		n /= 10;
 	}
-	nbr[0] = (n % 10) + 48;
 	return (nbr);
 }
