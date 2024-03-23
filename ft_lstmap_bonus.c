6631d33f797f09d180ea28c9eb6a3514abc2de49
Leo Agea <leoagea@gmail.com>
2024-03-23 23:15:37 +0100

    lstmap_bonus

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/23 23:05:46 by lagea             #+#    #+#             */
+/*   Updated: 2024/03/23 23:11:40 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list *new_lst;
+	t_list *new_node;
+
+	if (!lst)
+		return ;
+	new_lst = NULL;
+	while (lst)
+	{
+		if (f)
+			new_node = ft_lstnew(f(lst->content));
+		else
+			new_node = ft_lstnew(lst->content);
+		if (!new_node)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
+	}
+	return (new_lst);
+}
