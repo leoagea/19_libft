7ae867fb5feda59db96b432021eebb516a9e91d7
Leo Agea <leoagea@gmail.com>
2024-03-23 23:15:28 +0100

    lstlast_bonus

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 00:27:56 by lagea             #+#    #+#             */
-/*   Updated: 2024/03/23 00:30:35 by lagea            ###   ########.fr       */
+/*   Updated: 2024/03/23 22:37:01 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	while (lst->next != NULL && lst)
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
 		lst = lst->next;
 	return (lst);
 }
