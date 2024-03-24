17482a7d8ed26ff5e8b58e2f8d5c7e0e1c35c45a
Leo Agea <leoagea@gmail.com>
2024-03-25 00:35:04 +0100

    fixed KO

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 23:43:13 by lagea             #+#    #+#             */
-/*   Updated: 2024/03/21 01:06:46 by lagea            ###   ########.fr       */
+/*   Updated: 2024/03/25 00:33:26 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,26 @@
 
 size_t ft_strlcat(char * dst, const char * src, size_t dstsize)
 {
-    size_t i;
-    size_t j;
+    size_t	i;
+	size_t	j;
+	size_t	len_dst;
 
-    i = 0;
-    j = 0;
-    if (!dst || !src)
-        return 0;
-    while (dst [i])
-        i++;
-    while (src [i])
-    {
-        dst [i] = src [j];
-        i++;
-        j++;
-    }
-    if (j < dstsize)
-		dst[i] = '\0';
-    return (ft_strlen(src) + ft_strlen(dst));
+	i = 0;
+	j = 0;
+	len_dst = ft_strlen(dst);
+	if (!dst || !src)
+		return (0);
+	while (dst[i])
+		i++;
+	while (src[j] && (i + j + 1) < (dstsize))
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (j < dstsize)
+		dst[i + j] = '\0';
+	if (dstsize <= len_dst)
+		return (ft_strlen(src) + dstsize);
+	else
+		return (ft_strlen(src) + len_dst);
 }
