bed729f2912f70598b0cfff55b685e8993048919
Leo Agea <leoagea@gmail.com>
2024-03-25 00:16:44 +0100

    fixed KO

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,18 +6,23 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 21:29:34 by lagea             #+#    #+#             */
-/*   Updated: 2024/03/21 19:49:26 by lagea            ###   ########.fr       */
+/*   Updated: 2024/03/25 00:16:27 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 #include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void *res;
+
+	if (size != 0 && count > (-1 / size))
+		return (NULL);
 	res = (void *)malloc(count * size);
 	if (!res)
 		return (NULL);
-	ft_bzero(res, count * size);
-	return (res);
+	ft_bzero(res, count * size); 
+	return res;
 }
