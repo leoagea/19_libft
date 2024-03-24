1b37e878cbc341818a2829729128a93e4ac96052
Leo Agea <leoagea@gmail.com>
2024-03-25 00:49:42 +0100

    fixed KO

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 22:40:54 by lagea             #+#    #+#             */
-/*   Updated: 2024/03/21 01:07:39 by lagea            ###   ########.fr       */
+/*   Updated: 2024/03/25 00:49:20 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 
 	i = 0;
 	j = 0;
-	if (len == 0)
+	if (!str || !to_find)
 		return (NULL);
 	if (*to_find == '\0')
 		return ((char *)str);
 	while (str[i] && i < len)
 	{
 		j = 0;
-		while (str[i + j] == to_find[j] && to_find[j] && str[i + j])
+		while (str[i + j] == to_find[j] && str[i + j] && i + j < len)
 			j++;
 		if (j == ft_strlen(to_find))
 			return ((char *)str + i);
