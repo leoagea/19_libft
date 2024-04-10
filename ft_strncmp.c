54d876bd45c88f5c54a1c05c81022f80822bf90a
Leo Agea <leoagea@gmail.com>
2024-04-10 22:47:09 +0200

    fixed [fail]: your strncmp does not cast in unsigned char the diff [fail]: your strncmp doesn't stop at \0 [fail]: your strncmp does not work with non ascii chars

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 23:04:27 by lagea             #+#    #+#             */
-/*   Updated: 2024/04/10 12:55:17 by lagea            ###   ########.fr       */
+/*   Updated: 2024/04/10 22:46:14 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+/*int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
@@ -22,4 +22,20 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (s1[i] == s2[i] && i < n - 1)
 		i++;
 	return (s1[i] - s2[i]);
+}
+*/
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    if (n == 0)
+        return (0);
+    while (i < n)
+    {
+        if (s1[i] != s2[i] || s1[i] == '\0')
+            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+        i++;
+    }
+    return (0);
 }
