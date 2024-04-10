19e2da09fc366eaf72f03fd6b9f70aabcd0e5ff0
Leo Agea <leoagea@gmail.com>
2024-04-10 23:12:21 +0200

    fixed KO cast unsigned memory, Norme OK

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 21:45:42 by lagea             #+#    #+#             */
-/*   Updated: 2024/04/10 12:53:25 by lagea            ###   ########.fr       */
+/*   Updated: 2024/04/10 23:12:01 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	u_c;
 
+	u_c = (unsigned char)c;
 	i = 0;
 	while (i < n)
 	{
-		if (((unsigned char *)s)[i] == (char)c)
-			return (((unsigned char *)s) + i);
+		if (((unsigned char *)s)[i] == u_c)
+			return ((void *)((unsigned char *)s + i));
 		i++;
 	}
 	return (NULL);
