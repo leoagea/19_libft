9e43041f0db9fa87de72cdd089ed4509835dc4ff
Leo Agea <leoagea@gmail.com>
2024-04-10 22:47:49 +0200

    fixed, norme OK

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,36 +6,24 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 23:04:27 by lagea             #+#    #+#             */
-/*   Updated: 2024/04/10 22:46:14 by lagea            ###   ########.fr       */
+/*   Updated: 2024/04/10 22:47:36 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] == s2[i] && i < n - 1)
+	while (i < n)
+	{
+		if (s1[i] != s2[i] || s1[i] == '\0')
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
-	return (s1[i] - s2[i]);
-}
-*/
-int ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-    size_t i;
-
-    i = 0;
-    if (n == 0)
-        return (0);
-    while (i < n)
-    {
-        if (s1[i] != s2[i] || s1[i] == '\0')
-            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-        i++;
-    }
-    return (0);
+	}
+	return (0);
 }
