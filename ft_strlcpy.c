874e0801b1472adc7444d4735c4e80ad1427a52f
leoagea <leoagea@gmail.com>
2024-04-11 11:54:39 +0200

    supposed to segfault when null parameter

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 23:22:52 by lagea             #+#    #+#             */
-/*   Updated: 2024/04/10 12:56:03 by lagea            ###   ########.fr       */
+/*   Updated: 2024/04/11 11:53:58 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	i = 0;
-	if (!dst || !src)
-		return (0);
 	if (dstsize != 0)
 	{
 		while (i < dstsize - 1 && src[i])
