8a1a7d1411ae86a98db04ccdb2f294ec5a295198
leoagea <leoagea@gmail.com>
2024-04-11 15:31:28 +0200

    fixed KO Abort (forgot case n =0)

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 22:03:53 by lagea             #+#    #+#             */
-/*   Updated: 2024/04/11 15:08:09 by lagea            ###   ########.fr       */
+/*   Updated: 2024/04/11 15:30:19 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (n ==0)
+		return (0);
 	while (*(unsigned char *)(s1) == *(unsigned char *)(s2) && i < n - 1)
 	{
 		s1++;
