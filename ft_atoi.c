c94b4dfe0d5a1f95875f4c8be0ae664c3e833c26
Leo Agea <leoagea@gmail.com>
2024-04-12 23:47:59 +0200

    new func skip ws_sign

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,110 +3,90 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 19:07:17 by lagea             #+#    #+#             */
-/*   Updated: 2024/04/12 17:20:43 by lagea            ###   ########.fr       */
+/*   Updated: 2024/04/12 23:40:26 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
-static int	without_whitespace(char *str)
+// int	ft_atoi(const char *str)
+// {
+// 	int	i;
+// 	int	res;
+// 	int	sign;
+// 	int overflow;
+
+// 	i = 0;
+// 	res = 0;
+// 	sign = 1;
+// 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+// 		i++;
+// 	if (str[i] == '-')
+// 	{
+// 		sign = -1;
+// 		i++;
+// 	}
+// 	else if (str[i] == '+')
+// 		i++;
+// 	while (ft_isdigit((int)str[i]))
+// 	{
+// 		res += (str[i] - 48);
+// 		i++;
+// 		if (!(ft_isdigit((int)str[i])))
+// 			return (res * sign);
+// 		res *= 10;
+// 	}
+// 	return (res * sign);
+// }
+
+void	skip_whitespace_and_sign(const char **str, int *sign)
 {
-	int	i;
-
-	i = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
-	if (str[i] == '-')
-		i++;
-	else if (str[i] == '+')
-		i++;
-	else
-		return (i);
-}
-
-static int	check_sign(char *str)
-{
-	int	i;
-	int	sign;
-
-	i = 0;
-	sign = 1;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
-	if (str[i] == '-')
+	while ((**str >= 9 && **str <= 13) || **str == 32)
+		(*str)++;
+	if (**str == '-' || **str == '+')
 	{
-		i++;
-		sign = -1;
+		if (**str == '-')
+			*sign = -1;
+		(*str)++;
 	}
-	return (sign);
-}
-static int	check_overflow(char *str)
-{
-	int	i;
-	int	count;
-	int	sign;
-
-	sign = check_sign(str);
-	count = 0;
-	i = without_whitespace(str);
-	while (count < 10)
-	{
-		if (ft_isdigit((int)str[i]))
-		{
-			i++;
-			count++;
-		}
-		else
-			return (0);
-	}
-	if (ft_strlen(str + i) >)
 }
 
 int	ft_atoi(const char *str)
 {
-	int	i;
 	int	res;
-	int	sign;
-	int overflow;
+	int		sign;
+	long	overflow;
 
-	i = 0;
 	res = 0;
 	sign = 1;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
-	if (str[i] == '-')
+	skip_whitespace_and_sign(&str, &sign);
+	while (ft_isdigit(*str))
 	{
-		sign = -1;
-		i++;
-	}
-	else if (str[i] == '+')
-		i++;
-	while (ft_isdigit((int)str[i]))
-	{
-		res += (str[i] - 48);
-		i++;
-		overflow = res * res;
-		printf("overflow : %d\n",overflow);
-		if (overflow / res != res && sign == 1)
-			return -1;
-		else if (overflow / res != res && sign == -1)
-			return 0;
-		if (!(ft_isdigit((int)str[i])))
-			return (res * sign);
 		res *= 10;
+		res += (*str - 48);
+		str++;
+		overflow = res;
+		// printf("%lld ",overflow);
+		if (overflow * 10 > INT_MAX)
+			return (-1);
+		else if (overflow / 10< INT_MIN)
+			return 0;
 	}
-	return (res * sign);
+	return ((int)res * sign);
 }
+
 int	main(void)
 {
 	// char *s = "1221274aa9999999999999999999999aaaaa99999999999999999999999999999999999999999999999999999999999";
-	char *long_long = "9223372036854775807";
-	char *max_int = "2147483647";
-	char *test = "-9223372036854775805";
-	printf("%d\n", ft_atoi(test));
-	printf("%d\n", atoi(test));
+	// char *long_long = "9223372036854775807";
+	// char *max_int = "2147483647";
+
+	char *test = "-92233720368547758073232323";
+	printf("ft_atoi :%d\n", ft_atoi(test));
+	printf("atoi :%d\n", atoi(test));
 	return (0);
 }
